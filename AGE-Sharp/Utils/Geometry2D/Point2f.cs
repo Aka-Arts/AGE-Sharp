605059c890d79feb8025e5f13cb8e85a78bb9f23
Aka-Arts <alphaegli@gmail.com>
2015-05-01 15:49:33 +0200

    added solution with first classes and utils project

---
+++ AGE-Sharp/Utils/Geometry2D/Point2f.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Org.AkaArts.AgeSharp.Utils.Geometry2D
+{
+
+    public class Point2f
+    {
+
+        public float x;
+        public float y;
+
+        public Point2f()
+        {
+
+            this.x = 0;
+            this.y = 0;
+
+        }
+
+        public Point2f(float x, float y)
+        {
+
+            this.x = x;
+            this.y = y;
+
+        }
+
+    }
+
+}
