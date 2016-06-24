820045dd6be0a60b60631a8d30c81e4dda02310c
Aka-Arts <alphaegli@gmail.com>
2016-06-24 14:51:04 +0200

    Implemented demo for collision testing

--- TesterGraphical/CollisionTest/CollisionTest.cs
+++ TesterGraphical/CollisionTest/CollisionTest.cs
@@ -23,25 +23,35 @@ namespace TesterGraphical.CollisionTest
 
         SpriteFont font;
 
-        Polygon2D poly1;
-        Polygon2D poly2;
+        List<Polygon2D> worldObjects = new List<Polygon2D>();
+
+        Polygon2D player;
+        Polygon2D playerRequested;
+        Polygon2D playerEffective;
+        Polygon2D hull;
 
         Vector2 playerPosition; // poly1pos
-        Vector2 poly2position;
+        Vector2 playerRequestedPosition;
+
         Vector2 push = Vector2.Zero;
         double pushDist = 0;
+        double distTmp = 0;
 
-        Vector2 o2o = Vector2.Zero;
+        Vector2 player2poly2 = Vector2.Zero;
+        Vector2 player2poly3 = Vector2.Zero;
 
         bool playerIntersects = false;
+
+        KeyboardState previousKeys;
+        MouseState previousMouse;
 
         public CollisionTest()
             : base()
         {
             graphics = new GraphicsDeviceManager(this);
             graphics.IsFullScreen = false;
-            graphics.PreferredBackBufferHeight = 600;
-            graphics.PreferredBackBufferWidth = 1000;
+            graphics.PreferredBackBufferWidth = 500;
+            graphics.PreferredBackBufferHeight = 300;
 
             Content.RootDirectory = "Content";
         }
@@ -60,15 +70,33 @@ namespace TesterGraphical.CollisionTest
 
             this.Window.Title = "collision test";
 
+            previousKeys = Keyboard.GetState();
+            previousMouse = Mouse.GetState();
+
+            playerPosition = new Vector2(251, 51);
+
             var vertices = new List<Vector2>();
-            vertices.Add(new Vector2(30, -30));
             vertices.Add(new Vector2(-30, -30));
             vertices.Add(new Vector2(-30, 30));
             vertices.Add(new Vector2(30, 30));
+            vertices.Add(new Vector2(30, -30));
+            //vertices.Add(new Vector2(0, -30));
 
-            this.poly1 = new Polygon2D(GraphicsDevice, new Vector2(50, 50), vertices);
-            this.poly2position = new Vector2(500, 300);
-            this.poly2 = new Polygon2D(GraphicsDevice, poly2position, vertices);
+            this.player = new Polygon2D(GraphicsDevice, playerPosition, vertices);
+            this.playerRequested = new Polygon2D(GraphicsDevice, playerPosition, vertices);
+            this.playerEffective = new Polygon2D(GraphicsDevice, playerPosition, vertices);
+            var poly1position = new Vector2(200, 150);
+            var poly1 = new Polygon2D(GraphicsDevice, poly1position, vertices);
+            var poly2position = new Vector2(300, 250);
+            var poly2 = new Polygon2D(GraphicsDevice, poly2position, vertices);
+
+            worldObjects.Add(poly1);
+            worldObjects.Add(poly2);
+
+            var playerPoints = new List<Vector2>(player.AbsoluteVertices);
+            playerPoints.AddRange(playerRequested.AbsoluteVertices);
+
+            this.hull = new Polygon2D(GraphicsDevice, Vector2.Zero, Polygon2D.GetConvexHull(playerPoints));
 
             base.Initialize();
         }
@@ -103,26 +131,70 @@ namespace TesterGraphical.CollisionTest
         /// <param name="gameTime">Provides a snapshot of timing values.</param>
         protected override void Update(GameTime gameTime)
         {
-            if (GamePad.GetState(PlayerIndex.One).Buttons.Back == ButtonState.Pressed || Keyboard.GetState().IsKeyDown(Keys.Escape))
+            var currentKeys = Keyboard.GetState();
+            var currentMouse = Mouse.GetState();
+
+            if (GamePad.GetState(PlayerIndex.One).Buttons.Back == ButtonState.Pressed || currentKeys.IsKeyDown(Keys.Escape))
             {
                 Exit();
             }
 
-            var mouse = Mouse.GetState();
-            var mouseVector = mouse.Position.ToVector2();
-            var requiresUpdate = false;
-
-            if (!playerPosition.Equals(mouseVector) ||
-                Keyboard.GetState().IsKeyDown(Keys.F4))
+            if (this.IsActive)
             {
-                playerPosition = mouseVector;
-                requiresUpdate = true;
-                o2o = Vector2.Subtract(poly2.Origin, playerPosition);
-            }
+                var recalcHull = false;
 
-            if (requiresUpdate)
-            {
-                testCollisions();
+                if (previousKeys.IsKeyDown(Keys.Up) &&
+                    currentKeys.IsKeyUp(Keys.Up))
+                {
+                    playerPosition = playerPosition + new Vector2(0, -10);
+                    recalcHull = true;
+                }
+
+                if (previousKeys.IsKeyDown(Keys.Down) &&
+                    currentKeys.IsKeyUp(Keys.Down))
+                {
+                    playerPosition = playerPosition + new Vector2(0, 10);
+                    recalcHull = true;
+                }
+
+                if (previousKeys.IsKeyDown(Keys.Left) &&
+                    currentKeys.IsKeyUp(Keys.Left))
+                {
+                    playerPosition = playerPosition + new Vector2(-10, 0);
+                    recalcHull = true;
+                }
+
+                if (previousKeys.IsKeyDown(Keys.Right) &&
+                    currentKeys.IsKeyUp(Keys.Right))
+                {
+                    playerPosition = playerPosition + new Vector2(10, 0);
+                    recalcHull = true;
+                }
+                
+                var mouseVector = currentMouse.Position.ToVector2();
+
+                if (currentMouse.LeftButton == ButtonState.Pressed) //mouseVector != playerRequestedPosition)
+                {
+                    playerRequestedPosition = mouseVector;
+                    playerRequested.Origin = playerRequestedPosition;
+                    recalcHull = true;
+                }
+                else if (previousMouse.LeftButton == ButtonState.Pressed)
+                {
+                    playerPosition = playerEffective.Origin;
+                    player.Origin = playerPosition;
+                    recalcHull = true;
+                }
+
+                if (recalcHull || previousKeys.IsKeyDown(Keys.F4) && currentKeys.IsKeyUp(Keys.F4))
+                {
+                    player.Origin = playerPosition;
+                    calcPlayerHull();
+                    testCollisions();
+                }
+
+                previousKeys = currentKeys;
+                previousMouse = currentMouse;
             }
 
             base.Update(gameTime);
@@ -136,16 +208,21 @@ namespace TesterGraphical.CollisionTest
         {
             GraphicsDevice.Clear(Color.Black);
 
-            this.drawer.DrawLine(new LineSegment2D(poly2.Origin, playerPosition), Color.Cyan);
-            poly2.Draw(Color.LightSlateGray);
+            this.drawer.DrawLine(new LineSegment2D(playerPosition, playerRequestedPosition), Color.Cyan);
+
+            worldObjects.ForEach((x) => x.Draw(Color.SlateGray));
+
+            player.Draw(Color.Aquamarine);
+            playerRequested.Draw(Color.Aqua);
+            playerEffective.Draw(Color.DarkGoldenrod);
 
             if (playerIntersects)
             {
-                poly1.Draw(Color.Red);
+                hull.Draw(Color.Red);
             }
             else
             {
-                poly1.Draw(Color.Lime);
+                hull.Draw(Color.Lime);
             }
 
 
@@ -153,6 +230,8 @@ namespace TesterGraphical.CollisionTest
 
             spriteBatch.DrawString(font, "Push Vector: " + push, new Vector2(5, 5), Color.White);
             spriteBatch.DrawString(font, "Push Distance: " + pushDist, new Vector2(5, 25), Color.White);
+            spriteBatch.DrawString(font, "Player to Poly vector: " + player2poly2, new Vector2(5, 45), Color.White);
+            spriteBatch.DrawString(font, "distTmp: " + distTmp, new Vector2(5, 65), Color.White);
             //spriteBatch.DrawString(font, "Total visibility tests: " + tests, new Vector2(305, 5), Color.White);
             //spriteBatch.DrawString(font, "Total calculation time: " + calcTime + " ms", new Vector2(505, 5), Color.White);
             //spriteBatch.DrawString(font, "Multithreading (F12) enabled: " + useMultithreading, new Vector2(745, 5), useMultithreading ? Color.Lime : Color.White);
@@ -164,15 +243,35 @@ namespace TesterGraphical.CollisionTest
 
         void testCollisions()
         {
-            poly1.Origin = playerPosition;
-            var result = Polygon2D.Intersect(poly1, poly2, o2o);
-            playerIntersects = result.Intersects;
-            push = result.PushVector;
-            pushDist = result.PushDistance;
-            if (playerIntersects)
+            playerIntersects = false;
+
+            var direction = playerRequestedPosition - playerPosition;
+            var biggestPush = 0d;
+            var biggestPushVector = Vector2.Zero;
+
+            worldObjects.ForEach((x) =>
             {
-                poly1.Origin = playerPosition + result.PushVector;
-            }
+                var result = Polygon2D.Intersect(x, hull, direction);
+                playerIntersects = result.Intersects || playerIntersects;
+
+                if (result.PushDistance > biggestPush)
+                {
+                    biggestPush = result.PushDistance;
+                    biggestPushVector = result.PushVector;
+                    distTmp = result.distTmp;
+                }
+
+            });
+
+            push = biggestPushVector;
+            pushDist = biggestPush;
+
+            playerEffective.Origin = playerRequestedPosition + biggestPushVector;
+        }
+
+        void calcPlayerHull()
+        {
+            this.hull = player + playerRequested;
         }
     }
 }
