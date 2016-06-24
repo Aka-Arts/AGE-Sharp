8094571d3ba34c347337683da3985fc5b5ac529a
Aka-Arts <alphaegli@gmail.com>
2016-06-24 14:47:05 +0200

    added vector extension to determine if two vectors are orthogonal to each other

--- AkaArts.AgeSharp.Utils/Extensions.cs
+++ AkaArts.AgeSharp.Utils/Extensions.cs
@@ -1,4 +1,5 @@
-﻿using Microsoft.Xna.Framework.Audio;
+﻿using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Audio;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -68,5 +69,36 @@ namespace AkaArts.AgeSharp.Utils
 
         #endregion
 
+        #region VECTOR2
+
+        public static bool IsOrthogonalTo(this Vector2 v1, Vector2 v2)
+        {
+            var v1ToX = Math.Atan2(v1.Y, v1.X);
+            var v2ToX = Math.Atan2(v2.Y, v2.X);
+
+            if (v1ToX < 0)
+            {
+                v1ToX += Math.PI; // take the positive angle
+            }
+
+            if (v2ToX < 0)
+            {
+                v2ToX += Math.PI; // dito
+            }
+
+            var v1ToV2 = Math.Abs(v1ToX - v2ToX); // smallest between the 2 vectors
+
+            var epsilon = 1e-5;
+
+            if (v1ToV2 >= MathHelper.PiOver2 - epsilon && v1ToV2 <= MathHelper.PiOver2 + epsilon)
+            {
+                return true;
+            }
+            else{
+                return false;
+            }
+        }
+
+        #endregion
     }
 }
